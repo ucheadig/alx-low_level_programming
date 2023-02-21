b4d9eae7a0f82f094c0bf03b83095c7a78d4746f
Chinonso Adigwe <ucheadig@gmail.com>
2023-02-21 07:21:19 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-  *main - the entry point
-  *return - set to 0 at the end
+  * main - the entry point
+  *
+  * Return: set to 0 at the end
   */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
